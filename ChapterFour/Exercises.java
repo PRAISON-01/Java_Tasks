669e3926377cdf62a4e1f7b4b2a587a81ddf7000
Praise Nwankwo <nwankwopraison@gmail.com>
2026-04-23 09:00:08 +0100

    Added new files

---
+++ ChapterFour/Exercises.java
@@ -0,0 +1,73 @@
+/*
+
+1. if and while both use conditions. if checks runs once if true. while repeats while condition is true.
+
+
+
+2. Integer division removes decimals.
+
+7 / 2 = 3
+
+Use double to keep decimals.
+7.0 / 2 = 3.5
+
+
+3. Control statements combine by nesting (one inside another) or sequencing (one after another).
+
+
+
+4. User input until stop value uses sentinel-controlled repetition.
+Factorial of 5 uses counter-controlled repetition.
+
+
+
+5. x = 7, y = 3
+
+x = y++ + x
+x = 3 + 7 = 10
+
+x = ++y + x
+x = 4 + 7 = 11
+
+
+
+6. a)
+
+if(age >= 65)
+    System.out.println("Age is greater than or equal to 65");
+else
+    System.out.println("Age is less than 65");
+
+b)
+
+int x = 1, total = 0;
+
+while(x <= 10){
+    total += x;
+    x++;
+}
+
+c)
+
+while(x <= 100){
+    total += x;
+    x++;
+}
+
+d)
+
+while(y != 0){
+    System.out.println(y);
+    y--;
+}
+
+
+
+7.  Y is 0 and total is 0
+    Y is 1 and total is 1
+    Y is 2 and total is 3
+    Y is 3 and total is 6
+    Y is: 12 and total is 78
+
+
+*/
