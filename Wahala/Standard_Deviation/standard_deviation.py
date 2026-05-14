6cb76c40b1bce59c8122419e43c10640322e7d6a
Praise Nwankwo <nwankwopraison@gmail.com>
2026-05-14 06:33:16 +0100

    finished code that calculate fo the standard deviation of numbers

--- Wahala/Standard_Deviation/standard_deviation.py
+++ Wahala/Standard_Deviation/standard_deviation.py
@@ -7,9 +7,45 @@ def add(array):
 
     return total
 
-def mean_deviation(array):
+def mean(array):
     
     mean = add(array)/len(array)
+
     return mean
 
+def mean_deviation(array):
+    
+    total = 0
+    calc_mean = mean(array)
+    deviation = 0
+    for number in array:
+        deviation = abs(number - calc_mean)
+
+        total += deviation
+
+    return total/ len(array)
+
+def squared_deviation(array):
+    total = 0
+    calc_mean = mean(array)
+    deviation = 0
+    for number in array:
+        deviation = abs(number - calc_mean)
+
+        total += deviation * deviation
+
+    return total
+
+def variance(array):
+    dividend = float(squared_deviation(array))
+    divisor = float(len(array))
+
+    quotient = float(dividend / divisor)
+    return quotient
+
+def standard_dev(array):
+
+    sd = variance(array) ** 0.5
+    return sd
+    
 
--- Wahala/Standard_Deviation/standard_deviation_test.py
+++ Wahala/Standard_Deviation/standard_deviation_test.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
 
 from standard_deviation import add
+from standard_deviation import mean
+from standard_deviation import mean_deviation
+from standard_deviation import squared_deviation
+from standard_deviation import variance
+from standard_deviation import standard_dev
+#from standard_deviation import mean_deviation
+
 
 class TestStandardDeviation(TestCase):
     
@@ -14,6 +21,37 @@ class TestStandardDeviation(TestCase):
     def test_that_mean_method_returns_correct_mean(self):
     
         array = [1, 2, 3, 4, 5]
-        actual = add(array)
-        expected = 3
+        actual = mean(array)
+        expected = 3	
         self.assertEqual(actual, expected)
+
+    def test_that_mean_deviation_returns_correct_answer(self):
+
+        array = [1, 2, 3, 4, 5]
+        actual = mean_deviation(array)
+        expected = 1.2
+        self.assertEqual(actual, expected)
+
+    def test_that_squared_deviaiton_returnss_correct_value(self):
+
+        array = [1, 2, 3, 4, 5]
+        actual = squared_deviation(array)
+        expected = 10			
+        self.assertEqual(actual, expected)
+
+    def test_that_variance_returnss_correct_value(self):
+
+        array = [1, 2, 3, 4, 5]
+        actual = variance(array)
+        expected = 2			
+        self.assertEqual(actual, expected)
+
+    def test_that_statndard_deviation_returns_correct_score(self):
+
+        array = [1, 2, 3, 4, 5]
+        actual = standard_dev(array)
+        expected = 1.4142135623730951	
+        self.assertEqual(actual, expected)
+
+
+
